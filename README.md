215f818130c7bc562baa6b3eabb0d26be09945cc
Zane Helton <zane99@me.com>
2022-02-16 13:09:44 -0500

    Added install script & readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # macos-dotfiles
+Just a bundle of dotfiles I like for macOS
+
+- skhd
+- yabai
+- neofetch
+- macchina
+- kitty
+
+## Install
+
+`./install.sh`
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s $(pwd)/neofetch ~/.config/neofetch
+ln -s $(pwd)/kitty ~/.config/kitty
+ln -s $(pwd)/yabai ~/.config/yabai
+ln -s $(pwd)/skhd ~/.config/skhd
+ln -s $(pwd)/macchina ~/.config/macchina
