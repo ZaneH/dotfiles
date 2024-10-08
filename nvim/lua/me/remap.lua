f74890f79dcc6eb354cba5a9c127b0cc64e0a5ca
Zane Helton <zane99@me.com>
2024-10-08 16:17:48 -0400

    change oil --float to <leader>pV

--- nvim/lua/me/remap.lua
+++ nvim/lua/me/remap.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", function()
 	vim.cmd([[Oil]])
 end, { desc = "Open project view" })
-vim.keymap.set("n", "<leader>pf", function()
+vim.keymap.set("n", "<leader>pV", function()
 	vim.cmd([[Oil --float]])
 end, { desc = "Open project view (float)" })
 vim.keymap.set("i", "jk", "<Esc>")
