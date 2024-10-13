f049aa74d57de6aea9b8596890b4fd764146c6da
Zane Helton <zane99@me.com>
2024-10-13 17:30:20 -0400

    add desc to todo telescope

--- nvim/after/plugin/todo-comments.lua
+++ nvim/after/plugin/todo-comments.lua
@@ -1,3 +1,3 @@
 require("todo-comments").setup({})
 
-vim.keymap.set("n", "<leader>pt", "<cmd>TodoTelescope<cr>")
+vim.keymap.set("n", "<leader>pt", "<cmd>TodoTelescope<cr>", { desc = "Todo Telescope" })
